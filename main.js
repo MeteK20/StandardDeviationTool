0387023886b9c2a50c480b8ca51be73bdc4f04b7
Unknown <mete_kalak@hotmail.com>
2021-02-28 14:59:01 +0100

    More refactoring

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// importeer Statistics class, bij de path moet je met './' beginnen als je in dezelfde map zit 
+// importeer Statistics class
 import {Statistics} from './Statistics.js';
 
 // print de standaarddeviatie naar het scherm
@@ -8,26 +8,25 @@ function printStandardDeviation() {
     let inputField = document.getElementById('text-field');
     let values = '';
     /* zet de waarde van values op de input van de form
-        gebruik value i.p.v. innerHTML wanneer het een input element betreft
+        gebruik value i.p.v. innerText wanneer het een input element betreft
     */
     values = inputField.value;
+    // empty check
+    if (values === null || values === '') {
+        errorMsg(inputField, 'De invoer mag niet leeg zijn.');
+        return;
+    }
     // zet values om naar een array, bij elke komma in de string
     values = values.split(',');
     // verwijder spaties uit elke string van values en converteer ze naar getallen
     for(let i = 0; i < values.length; i++) {
         console.log(typeof values[i]);
         values[i] = values[i].trim();
-        try {
-            values[i] = Number(values[i]);
-            console.log(typeof values[i]);
-        }
-        catch (err) {
-            return;
-        }
-        // invoer validatie check
+        values[i] = parseInt(values[i]);
+        console.log(typeof values[i]);
+        // getal check -> TODO: wijzigen naar regex om hele invoer te checken
         if (!Number.isInteger(values[i])) {
-            inputField.classList.add('border-danger', 'text-danger');
-            inputField.value = 'Voer alleen getallen in.';
+            errorMsg(inputField, 'Voer alleen getallen in.');
             return;
         }
         
@@ -37,6 +36,12 @@ function printStandardDeviation() {
     let sd = stats.calcStandardDeviation(values);
     // print standaarddeviatie
     document.getElementById('output').innerHTML = sd;
+}
+
+// geef foutmelding
+function errorMsg(element, message) {
+    element.classList.add('border-danger', 'text-danger');
+    element.value = message;
 }
 
 // invoer en foutmelding clear
